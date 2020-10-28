0c0d5c728cf3f5f808381ae9aa001299017fd40e
Andrea Fuggetta <afuggetta@gmail.com>
2020-10-28 16:59:11 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -4,7 +4,7 @@
 
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "pg-network"
     workspaces {
       name = "hashicat-azure"
